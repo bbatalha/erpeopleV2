e6a8873af4cf826ce01e978617375d389fd6f3c9
Bruno Batalha <bbatalha@gmail.com>
2025-01-21 17:45:10 -0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['@mui/material', '@mui/x-data-grid', '@mui/x-date-pickers'],
+          charts: ['recharts'],
+          utils: ['date-fns', 'html2canvas', 'jspdf']
+        }
+      }
+    }
   },
+  envPrefix: 'VITE_',
+  server: {
+    watch: {
+      usePolling: true
+    }
+  }
 })
